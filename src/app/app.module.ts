89b8ab814e7bab578475acaba80b2fe7cf9e15c4
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2020-04-29 11:42:32 -0400

    bootstrapworks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
